02149e706e61bd93bd0cb3a0d4f87ac3aaa9ce51
Raijo Cheruvathoor <raijocheruvathoor@gmail.com>
2025-05-22 15:35:56 +0530

    changed cores origins

--- BackEnd/smartlearn_api/smartlearn_api/settings.py
+++ BackEnd/smartlearn_api/smartlearn_api/settings.py
@@ -131,12 +131,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "https://mysmartlearn.com",  
-#      "https://www.mysmartlearn.com", 
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "https://mysmartlearn.com",  
+     "https://www.mysmartlearn.com", 
+]
    
 
 # Internationalization
