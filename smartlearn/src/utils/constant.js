2acd87c1c99d59ad9f0e41b31210303f0cbcc45a
Raijo Cheruvathoor <raijocheruvathoor@gmail.com>
2025-06-03 21:13:56 +0530

    Url Change

--- smartlearn/src/utils/constant.js
+++ smartlearn/src/utils/constant.js
@@ -1,2 +1,2 @@
-//xport const baseUrl='https://mysmartlearn.com/api/'
-export const baseUrl='http://127.0.0.1:8000/api/'
+export const baseUrl='https://mysmartlearn.com/api/'
+//export const baseUrl='http://127.0.0.1:8000/api/'
