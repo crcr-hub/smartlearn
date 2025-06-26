11288b53d88324a077dd4582dfa904bf4a8e5f64
Raijo Cheruvathoor <raijocheruvathoor@gmail.com>
2025-06-26 10:52:56 +0530

    Updated cors allow origins

--- BackEnd/smartlearn_api/smartlearn_api/settings.py
+++ BackEnd/smartlearn_api/smartlearn_api/settings.py
@@ -132,12 +132,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "https://mysmartlearn.com",  
-#      "https://www.mysmartlearn.com", 
-# ]
+CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOWED_ORIGINS = [
+    "https://mysmartlearn.com",  
+     "https://www.mysmartlearn.com", 
+]
    
 
 # Internationalization
